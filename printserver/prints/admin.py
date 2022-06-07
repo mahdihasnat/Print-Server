a232678096c10eac6707a3b7d713e1a164635899
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2022-06-07 21:11:48 +0600

    Added prints model

---
+++ printserver/prints/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Prints
+
+@admin.register(Prints)
+class PrintsAdmin(admin.ModelAdmin):
+	list_display = ('print_id', 'owner', 'tag', 'source_code', 'submission_time', 'printing_time', 'total_page', 'status')
+	list_filter = ('owner', 'status','submission_time', 'total_page')
+	search_fields = ('print_id', 'owner', 'tag', 'source_code', 'submission_time', 'printing_time', 'total_page', 'status')
+	ordering = ('-submission_time',)
---
+++ printserver/prints/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class Prints(models.Model):
+
+	class Status(models.IntegerChoices):
+		SENT = 1
+		QUEUED = 2
+		PRINTING = 3
+		PRINTED = 4
+		DELIVERING = 5
+		DELIVERED = 6
+
+	print_id = models.AutoField(primary_key=True)
+	owner = models.ForeignKey('users.TeamUser', on_delete=models.CASCADE , related_name='prints')
+	tag = models.CharField(max_length=100)
+	source_code = models.TextField()
+
+	submission_time = models.DateTimeField(auto_now_add=True)
+	printing_time = models.DateTimeField(null=True)
+
+	total_page = models.IntegerField(null=True)
+	status = models.IntegerField(choices=Status.choices, default=Status.SENT)
