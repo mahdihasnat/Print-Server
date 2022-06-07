0f0c1aee3151f923e0a8d610571710007ac4efeb
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2022-06-07 12:41:02 +0600

    Added user model

--- printserver/users/models.py
+++ printserver/users/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin,AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
+class User(AbstractBaseUser,PermissionsMixin):
+	"""
+		username and password are required. Other fields are optional.
+	"""
+	username_validator = UnicodeUsernameValidator()
 
-# Create your models here.
+	username = models.CharField(
+        max_length=150,
+        unique=True,
+        help_text=_(
+            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
+        ),
+        validators=[username_validator],
+        error_messages={
+            "unique": "A user with that username already exists.",
+        },
+  )
+	team_name = models.CharField(
+		max_length=255,
+		verbose_name='Team Name',
+	)
+	location = models.CharField(
+		max_length=255,
+		verbose_name='Location'
+	)
+
+	USERNAME_FIELD = 'username'
+	REQUIRED_FIELDS = [] #['team_name','location']
