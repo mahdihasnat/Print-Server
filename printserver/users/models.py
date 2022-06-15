1c90d7e4066fac06c590ba81c129b19ac844c62e
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2022-06-15 12:26:59 +0600

    import send_mail

--- printserver/users/models.py
+++ printserver/users/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser , AbstractBaseUser,Permissio
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
+from django.core.mail import send_mail
 
 class MyUser(AbstractBaseUser, PermissionsMixin):
 	"""
