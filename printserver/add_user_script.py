f1c05d5f065ae1a5225b3c48d30d508b6b1877b4
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2022-06-15 17:42:05 +0600

    added printer group

--- printserver/add_user_script.py
+++ printserver/add_user_script.py
@@ -1,6 +1,30 @@
 from users.models import MyUser, TeamUser, PrinterUser
-# Import DoesnotExist exception from django.db.models.base
+from django.contrib.auth.models import Group , Permission
 
+PRINTER_GROUP_NAME = 'printer'
+
+def create_printer_group():
+	try:
+		group = Group.objects.get(name=PRINTER_GROUP_NAME)
+	except Group.DoesNotExist:
+		group = Group.objects.create(name=PRINTER_GROUP_NAME)
+	
+	group.permissions.add(Permission.objects.get(name='Can add prints'))
+	group.permissions.add(Permission.objects.get(name='Can change prints'))
+	group.permissions.add(Permission.objects.get(name='Can delete prints'))
+	group.permissions.add(Permission.objects.get(name='Can view prints'))
+	group.save()
+
+
+def add_groups(user,groupname):
+	try:
+		group = Group.objects.get(name=groupname)
+	except Group.DoesNotExist:
+		assert(False)
+
+		# Add can view prints to group
+	group.user_set.add(user)
+	group.save()
 
 def add_team(username, password, team_name, location):
 	try:
@@ -27,11 +51,13 @@ def add_printer(username, password):
 	user.set_password(password)
 	user.is_team = False
 	user.is_printer = True
+	user.is_staff = True
 	user.save()
 	try:
 		printer = PrinterUser.objects.get(user=user)
 	except PrinterUser.DoesNotExist:
 		printer = PrinterUser.objects.create(user=user)
+	add_groups(user, PRINTER_GROUP_NAME)
 	printer.save()
 
 def add_superuser(username, password):
@@ -42,6 +68,9 @@ def add_superuser(username, password):
 	user.set_password(password)
 	user.save()
 
+create_printer_group()
 add_superuser('admin', 'admin')
 add_team("t1","t1","Team one","DBL")
 add_printer("p1","p1")
+
+print("All data added")
--- printserver/init.sh
+++ printserver/init.sh
@@ -1,3 +1,4 @@
-python manage.py makemigrations
+python manage.py makemigrations prints users
 python manage.py migrate
+python manage.py shell < add_user_script.py
 python manage.py runserver
