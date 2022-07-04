9cedc5d0f76ee46e5645d20fd5eb54f5f40a7163
Rafid Bin Mostofa <rebornplusplus@gmail.com>
2022-07-05 01:50:08 +0600

    Added Context Processor and Refactored includes/css,js to server static locally

--- printserver/printserver/settings.py
+++ printserver/printserver/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'printserver.context_processor.project_constants',  # project constants
             ],
         },
     },
