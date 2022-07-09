04cdbc657f71cca6ceea979bdfc7c514907c4462
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2022-07-09 09:15:23 +0600

    Removed Multiple Declaration of STATIC_URL

--- printserver/printserver/settings.py
+++ printserver/printserver/settings.py
@@ -76,6 +76,7 @@ TEMPLATES = [
     },
 ]
 
+# Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'collectedstatic'
@@ -130,10 +131,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
