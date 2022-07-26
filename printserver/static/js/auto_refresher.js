dd2914a54f98380456fbb62fe42e62587d6d2cbf
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2022-07-26 07:44:42 +0600

    auto refresh every 60 sec

--- printserver/static/js/auto_refresher.js
+++ printserver/static/js/auto_refresher.js
@@ -3,7 +3,7 @@ function refreshPage()
 	if(window.scrollY == 0)
 		window.location = window.location.href;
 }
-var interval = 5*1000;
+var interval = 60*1000;
 setTimeout(setInterval('refreshPage()', interval),interval);
 console.log("auto_refresher.js loaded");
 console.log("Scroll down to stop refreshing page");
