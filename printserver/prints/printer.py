0afee70507153fa98ceec6f491b449f5519d6737
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2022-07-26 07:34:52 +0600

    tested printer

--- printserver/prints/printer.py
+++ printserver/prints/printer.py
@@ -2,6 +2,7 @@ from .models import Prints
 from .pdf_gen import get_pdf, save_pdf
 from django.conf import settings
 
+import subprocess
 
 def save_to_file(prints):
 	# returns path to file
@@ -15,8 +16,17 @@ def save_to_file(prints):
 def do_prints(prints):
 	file_path = save_to_file(prints)
 	# print( "file path: " , file_path)
+	p = subprocess.Popen(['lpr',str(file_path)], stdout=subprocess.PIPE)
+	(out,err) = p.communicate()
+	print("Error Stream:",err)
+	print("Output Stream:",out)
+	if len(out)> 0:
+		return False
+	else:
+		prints.status = Prints.Status.PRINTING
+		prints.save()
+		return True
 	
-
-
 def add_prints(prints):
-	do_prints(prints)
+	if do_prints(prints) == False:
+		raise Exception("error auto printing")
