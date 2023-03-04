c85b212902eed72710b5a449f1f7bc7bb2d33bcf
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2023-03-04 13:52:08 +0600

    added deploy notes and bash script

---
+++ deploy-notes.md
@@ -0,0 +1,10 @@
+Required Applications: python3,
+
+Steps:
+1. Install dependencies: `pip3 install -r requirements.txt`	
+2. * Make migrations: `python3 ./printserver/manage.py makemigrations`
+	*  Migrate: `python3 ./printserver/manage.py migrate`
+3. Collect static files: `python3 ./printserver/manage.py collectstatic`
+4. Add user to database: `python3 printserver/manage.py shell < printserver/add_user_script.py`
+5. Follow instructions in [staticserver/Readme.md](staticserver/Readme.md) to setup static server.
+6. Run server: `python3 ./printserver/manage.py runserver`
---
+++ deploy.sh
@@ -0,0 +1,48 @@
+PIPCMD="pip"
+PYTHONCMD="python3"
+# set -x # prints commands as they are executed
+
+# dependencies for printserver
+$PIPCMD install -r requirements.txt
+
+# make migrations
+$PYTHONCMD printserver/manage.py makemigrations users prints
+$PYTHONCMD printserver/manage.py migrate
+
+# Add user to database
+$PYTHONCMD printserver/manage.py shell < printserver/add_user_script.py
+
+# install nginx
+sudo apt install nginx-full
+
+# Write config file for nginx
+current_dir=$(pwd)
+echo "current_dir: $current_dir"
+nginx_config="server {
+	listen 8001;
+	location /collectedstatic/ {
+		root $current_dir/printserver/;
+	}
+	add_header Access-Control-Allow-Origin *;
+}"
+nginx_config_file="/etc/nginx/conf.d/pstatic.conf"
+touch $nginx_config_file
+chmod +w $nginx_config_file
+echo $nginx_config > $nginx_config_file
+
+# Restart nginx
+nginx -s reload
+
+# test nginx
+x=$(curl -X GET http://localhost:8001/collectedstatic/js/all.js)
+if [ -z "$x" ]; then
+	echo "nginx not working"
+	exit 1
+fi
+
+echo "Chanage static_url in /printserver/printserver/settings.py to http://IP_FOR_STATICSERVER:8001/collectedstatic/"
+
+# Run server
+$PYTHONCMD printserver/manage.py runserver
+
+
